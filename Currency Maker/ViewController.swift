a753047d5f7ab443175a48c11eb0ba95b77d1409
Amin Jalalian <aminjalalian1993@gmail.com>
2018-08-13 09:43:29 +0430

    Initial Commit

---
+++ Currency Maker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Currency Maker
+//
+//  Created by amin jalalian on 5/22/1397 AP.
+//  Copyright © 1397 Amin.Jalalian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
